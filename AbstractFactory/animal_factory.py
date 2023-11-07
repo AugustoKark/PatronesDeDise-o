dfd3a9c7fa3b304324a4dfc44f66639ae525fa5d
AugustoKark <102602307+AugustoKark@users.noreply.github.com>
2023-11-07 13:36:06 -0300

    Abstract Factory

---
+++ AbstractFactory/animal_factory.py
@@ -0,0 +1,24 @@
+from animales.dog import Dog
+from animales.cat import Cat
+from animales.animal import Animal
+from abc import ABC, abstractmethod
+
+
+class AnimalFactory(ABC):
+    @abstractmethod
+    def create_animal(self, type):
+        pass
+
+class DogFactory(AnimalFactory):
+    def create_animal(self, type):
+        if type == "dog":
+            return Dog()
+        else:
+            raise ValueError("Invalid animal type")
+
+class CatFactory(AnimalFactory):
+    def create_animal(self, type):
+        if type == "cat":
+            return Cat()
+        else:
+            raise ValueError("Invalid animal type")
