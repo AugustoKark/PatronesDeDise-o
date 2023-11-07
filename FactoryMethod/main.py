9e93b8847581946e55bebad22ed9285a8dbce010
AugustoKark <102602307+AugustoKark@users.noreply.github.com>
2023-11-07 12:56:43 -0300

    Delete FactoryMethod directory

--- FactoryMethod/main.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from triangulofactorymethod import TrianguloFactoryMethod
-
-if __name__ == '__main__':
-    metodo = TrianguloFactoryMethod()
-    triangulo = metodo.create_triangulo(10, 10, 10)
-    print(triangulo.get_descripcion())
